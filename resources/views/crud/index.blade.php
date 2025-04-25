92571be5f90848864f452cc8b9262862e5396b60
04-velasco-mariana <22151240@aguascalientes.tecnm.mx>
2025-04-25 12:55:21 -0600

    feat:✨:routes introduction

---
+++ resources/views/crud/index.blade.php
@@ -0,0 +1,16 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Crud</h1>
+    <h3> <?php echo $name ?> </h3>
+    <h3> {{$name}} </h3>
+   
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route; // route clase para manejar las rutas
 
+//rutas 
 Route::get('/', function () {
-    return view('welcome');
 });
+
+// Route::get('/test', function (){
+// return "Welcome";
+// });
+
+Route::get('/test', function (){
+    return  view( 'test' ); 
+    });
+
+    // Route::get('/crud', function (){
+    //     return  view( 'crud/index' ); 
+    //     });
+
+
+Route::get('/crud', function (){
+    $age=20;
+
+    $data =['name' => 'Mariana', 'age' => $age];
+
+         return view( 'crud/index', $data ); 
+ })-> name('crud-route'); //nombre de la ruta
+
+ 
+
+
