e2e7b1491509769d9bcdcf1a2d3bd5268eb45a4f
04-velasco-mariana <22151240@aguascalientes.tecnm.mx>
2025-04-30 10:50:05 -0600

    feat:✨:changes with laravel routes

--- routes/web.php
+++ routes/web.php
@@ -4,28 +4,47 @@ use Illuminate\Support\Facades\Route; // route clase para manejar las rutas
 
 //rutas 
 Route::get('/', function () {
+    return view ('welcome');
 });
+
+Route::get('/contact', function () {
+    return view ('contact');
+}) -> name ('contact');
+
+Route::get('/contact2', function () {
+    return view ('contact2');
+}) -> name ('contact2');
+
+
+
+
+
+
+
+
+
+
 
 // Route::get('/test', function (){
 // return "Welcome";
 // });
 
-Route::get('/test', function (){
-    return  view( 'test' ); 
-    });
+// Route::get('/test', function (){
+//     return  view( 'test' ); 
+//     });
 
     // Route::get('/crud', function (){
     //     return  view( 'crud/index' ); 
     //     });
 
 
-Route::get('/crud', function (){
-    $age=20;
+// Route::get('/crud', function (){
+//     $age=20;
 
-    $data =['name' => 'Mariana', 'age' => $age];
+//     $data =['name' => 'Mariana', 'age' => $age];
 
-         return view( 'crud/index', $data ); 
- })-> name('crud-route'); //nombre de la ruta
+//          return view( 'crud/index', $data ); 
+//  })-> name('crud-route'); //nombre de la ruta
 
  
 
